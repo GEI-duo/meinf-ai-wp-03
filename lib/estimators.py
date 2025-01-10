95731367d2f6fad5c816e51555a3591828d3bb0b
drownedd <drwn.it@gmail.com>
2025-01-10 09:44:58 +0000

    Some iterations

--- lib/estimators.py
+++ lib/estimators.py
@@ -2,6 +2,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.base import TransformerMixin, BaseEstimator
 import pandas as pd
 
+
 class CustomImputer(BaseEstimator, TransformerMixin):
     def __init__(self, columns, strategy="mean"):
         self.columns = columns
@@ -9,25 +10,26 @@ class CustomImputer(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         if self.strategy == "mean":
-            self.imputer = SimpleImputer(strategy='mean')
+            self.imputer = SimpleImputer(strategy="mean")
         elif self.strategy == "median":
-            self.imputer = SimpleImputer(strategy='median')
+            self.imputer = SimpleImputer(strategy="median")
         elif self.strategy == "mode":
-            self.imputer = SimpleImputer(strategy='most_frequent')
+            self.imputer = SimpleImputer(strategy="most_frequent")
         elif self.strategy == "constant":
-            self.imputer = SimpleImputer(strategy='constant', fill_value='Unknown')
+            self.imputer = SimpleImputer(strategy="constant", fill_value="Unknown")
         else:
             raise ValueError("Invalid strategy")
         self.imputer.fit(X[self.columns])
         return self
-    
+
     def transform(self, X):
         X = X.copy()
         X[self.columns] = self.imputer.transform(X[self.columns])
         return X
-    
+
     def get_feature_names_out(self, input_features=None):
         return input_features
+
 
 class CustomMappingEncoder(BaseEstimator, TransformerMixin):
     def __init__(self, mapping):
@@ -41,9 +43,10 @@ class CustomMappingEncoder(BaseEstimator, TransformerMixin):
         for col, map in self.mapping.items():
             X[col] = X[col].map(map)
         return X
-    
+
     def get_feature_names_out(self, input_features=None):
         return input_features
+
 
 class CustomDateEncoder(BaseEstimator, TransformerMixin):
     def __init__(self, date_columns):
@@ -51,16 +54,17 @@ class CustomDateEncoder(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X):
         X = X.copy()
         for col in self.date_columns:
             new_col = col + "_Year"
             X[new_col] = pd.to_datetime(X[col]).dt.year
         return X
-    
+
     def get_feature_names_out(self, input_features=None):
         return input_features
+
 
 class CustomDropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -73,6 +77,6 @@ class CustomDropColumns(BaseEstimator, TransformerMixin):
         X = X.copy()
         X = X.drop(columns=self.columns)
         return X
-    
+
     def get_feature_names_out(self, input_features=None):
         return input_features
